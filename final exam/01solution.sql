9babed8ed2e11679ef3bcc324568e747ea5517e1
yufk4 <96722170+yufk4@users.noreply.github.com>
2022-03-03 18:09:40 +0800

    Create 01solution.sql

---
+++ final exam/01solution.sql
@@ -0,0 +1,9 @@
+Q1. The Production.Product table contains a column named Name and a column named Price. You want to write a query that returns the values in these columns. In your resultset, you want the values in the Name column to be returned in a column named ProductName. Which two of the following SELECT statements can you use?
+
+a. SELECT Name AS ProductName, Price FROM Production.Product;
+b. SELECT ProductName, Price FROM Production.Product;
+c. SELECT Name ProductName, Price FROM Production.Product;
+d. SELECT Name, ProductName, Price FROM Production.Product;
+e. SELECT Name, Price FROM Production.Product AS ProductName;
+
+# Answer: a,c
