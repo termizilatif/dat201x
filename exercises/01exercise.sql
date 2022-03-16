29bf0834c74191d22a9332c0d8790a2041a16e6a
TermiziLatif <96722170+termizilatif@users.noreply.github.com>
2022-03-17 04:17:08 +0800

    Create 01exercise.sql

---
+++ exercises/01exercise.sql
@@ -0,0 +1,10 @@
+< < Retrieving Customer Data > >
+
+AdventureWorks Cycles is a company that sells directly to retailers, who then sell products to consumers. Each retailer that is an AdventureWorks customer has provided a named contact for all communication from AdventureWorks.
+
+The sales manager at AdventureWorks has asked you to generate some reports containing details of the company's customers to support a direct sales campaign. Let's start with some basic exploration.
+
+# Familiarize yourself with the Customer table by writing a Transact-SQL query that retrieves all columns for all customers.
+
+SELECT *
+FROM SalesLT.Customer;
